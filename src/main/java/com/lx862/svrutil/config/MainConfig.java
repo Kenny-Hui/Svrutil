05945af61524c648e14a2836f149048b28e0e039
LX862 <kenny.mh.hui@outlook.com>
2024-09-07 16:04:22 +0800

    Remove silentkick
    The vanilla /kick and /fancykick should have already served the same purpose

--- src/main/java/com/lx862/svrutil/config/MainConfig.java
+++ src/main/java/com/lx862/svrutil/config/MainConfig.java
@@ -1,12 +1,9 @@
 package com.lx862.svrutil.config;
 
 import com.google.gson.*;
-import com.lx862.svrutil.Mappings;
 import com.lx862.svrutil.ModInfo;
 import com.lx862.svrutil.SvrUtil;
-import com.lx862.svrutil.data.JoinMessage;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,7 +11,6 @@ import java.util.*;
 
 public class MainConfig {
     private static final Path CONFIG_PATH = Config.getConfigPath("config.json");
-    private static Text silentKickMessage = Mappings.literalText("Internal Exception: java.lang.StackOverflowError");
     private static boolean fixedItemFrame = false;
     private static int minItemFrameInteractOpLevel = 0;
     private static int fallingBlockDelay = 2; // Vanilla takes 2 tick to fall
@@ -44,19 +40,6 @@ public class MainConfig {
                 }
             }
 
-            if(jsonConfig.has("silentKickMessage")) {
-                JsonElement element = jsonConfig.get("silentKickMessage");
-                try {
-                    silentKickMessage = Text.Serializer.fromJson(element.getAsString());
-                } catch (Exception ignored) {
-                }
-
-                try {
-                    silentKickMessage = Text.Serializer.fromJson(element);
-                } catch (Exception ignored) {
-                }
-            }
-
             if(jsonConfig.has("fixedItemFrame")) {
                 fixedItemFrame = jsonConfig.get("fixedItemFrame").getAsBoolean();
             }
@@ -80,15 +63,10 @@ public class MainConfig {
     public static void generate() {
         SvrUtil.LOGGER.info("[{}] Generating config...", ModInfo.MOD_NAME);
         final JsonObject jsonConfig = new JsonObject();
-        final JsonArray welcomeConfig = new JsonArray();
-        final JsonObject welcomeConfig1 = JoinMessage.toJson(new JoinMessage(Mappings.literalText("Welcome title").formatted(Formatting.GREEN), Mappings.literalText("Welcome subtitle").formatted(Formatting.AQUA), Mappings.literalText("Please edit \"config/svrutil/config.json\" to change the welcome message.\n\nThank you for installing SvrUtil.").formatted(Formatting.GREEN), 20, Arrays.asList(1, 2, 3, 4)));
-        welcomeConfig.add(welcomeConfig1);
         jsonConfig.addProperty("fallingBlockDelay", fallingBlockDelay);
         jsonConfig.addProperty("fixedItemFrame", fixedItemFrame);
         jsonConfig.addProperty("minItemFrameInteractOpLevel", minItemFrameInteractOpLevel);
         jsonConfig.addProperty("whitelistedMessage", "You are not whitelisted on the server!");
-        jsonConfig.addProperty("silentKickMessage", "Internal Exception: java.lang.StackOverflowError");
-        jsonConfig.add("joinMessages", welcomeConfig);
 
         try {
             Files.write(CONFIG_PATH, Collections.singleton(new GsonBuilder().setPrettyPrinting().create().toJson(jsonConfig)));
@@ -105,9 +83,5 @@ public class MainConfig {
     }
     public static int getMinItemFrameInteractOpLevel() {
         return minItemFrameInteractOpLevel;
-    }
-
-    public static Text getSilentKickMessage() {
-        return silentKickMessage;
     }
 }
